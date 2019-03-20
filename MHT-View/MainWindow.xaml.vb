5840596b71cee2bdfcca9b52077644750bcac1ef
String.Empty <akauffman@cmm-measure.com>
2019-03-20 10:49:32 -0700

    Set mhtPath using a function instead of the side-effect of a sub

--- MHT-View/MainWindow.xaml.vb
+++ MHT-View/MainWindow.xaml.vb
@@ -1,13 +1,13 @@
 ﻿Class MainWindow
     ' This holds the path/URL passed in via the command line
-    Private _mhtPath As String
+    Private ReadOnly _mhtPath As String
 
     Public Sub New()
         ' This call is required by the designer.
         InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
-        ProcessCommandLineArgs()
+        _mhtPath = ProcessCommandLineArgs()
     End Sub
 
     Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
@@ -48,12 +48,14 @@
         If (e.ChangedButton = MouseButton.Left) Then DragMove()
     End Sub
 
-    Private Sub ProcessCommandLineArgs()
+    Private Shared Function ProcessCommandLineArgs() As String
         Dim commandLineArgs() As String
         commandLineArgs = Environment.GetCommandLineArgs
+        Dim mhtFile As String = String.Empty
 
         ' Argument validation
         Dim errorMessage As String = Nothing
+
         '   Must have exactly one argument
         If (commandLineArgs Is Nothing OrElse commandLineArgs.LongLength <> 2) Then
             errorMessage = "Invalid number of command line arguments." & Environment.NewLine _
@@ -68,7 +70,7 @@
         ElseIf Not commandLineArgs(1).Substring(commandLineArgs(1).Length - 4) = ".mht" Then
             errorMessage = "This is not an *.mht file: " & Environment.NewLine & commandLineArgs(1)
         Else ' Accept one argument as a path to the MHT file
-            _mhtPath = commandLineArgs(1)
+            mhtFile = commandLineArgs(1)
         End If
 
         ' If we ran in to some problems we should dump an error message and quit before we even load the form
@@ -76,5 +78,7 @@
             Console.WriteLine(errorMessage)
             Application.Current.Shutdown()
         End If
-    End Sub
+
+        Return mhtFile
+    End Function
 End Class
